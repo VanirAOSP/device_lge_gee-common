e0b2baa434e67f0c1fe2861c21fdc826f7c35e41
DHOMD <DHO@dho.im>
2014-12-27 15:11:18 -0500

    Gee not mako

--- liblight/lights.c
+++ liblight/lights.c
@@ -347,7 +347,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "mako lights module",
+    .name = "Gee lights module",
     .author = "Google, Inc., AOKP",
     .methods = &lights_module_methods,
 };
